72164e11fe063b04ad3a648560cfc290157fd468
zakariae elmoufid <moufidzakarie3@gmail.com>
2024-11-15 09:47:56 +0100

    Séparer et trier les nombres pairs et impairs

--- challenge4.js
+++ challenge4.js
@@ -1,16 +1,29 @@
 let mainArray = [12,3,-5,8,15,38,-16];
 
 function separateAndSort(){
+
+    for(let i = 0 ; i < mainArray.length ; i++){
+        for(let j = i+1 ; j <mainArray.length ; j++){
+            if(mainArray[i] > mainArray[j]){
+                let swap = mainArray[i] ;
+                mainArray[i] = mainArray[j];
+                mainArray[j] = swap ;
+            }
+        }
+    }
+   
    let peersArray = [];
    let oddArray = [];
+
    mainArray.forEach(element => {
-    if(element%2 == 0 ){
+    if(element%2 == 0){
         peersArray.push(element);
     }else {
         oddArray.push(element);
     }
-    console.log(peersArray );
-    console.log(oddArray);
 });
+console.log(peersArray );  
+
+console.log(oddArray);
 }
 separateAndSort()
